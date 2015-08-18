fff3fef8e71f089bb350cfee242006c36bd969e2
Amir Pouya Aghasadeghi <amirpouya.a@gmail.com>
2015-08-18 15:01:43 +0430

    Corrected number of arg from 2 to 3.

--- remove_english.py
+++ remove_english.py
@@ -6,8 +6,7 @@ import sys
 arabic_file=''
 farsi_file=''
 
-if len(sys.argv)!=2:
-    print 'Error:'
+if len(sys.argv)!=3:
     print 'Usagae: python remove_english.py arabice_file_path  farsi_file_path'
     exit(-1)
 
@@ -17,11 +16,14 @@ farsi_file=sys.argv[2]
 ar_file=io.open(arabic_file,'r',encoding='utf-8')
 fa_file=io.open(farsi_file,'r',encoding='utf-8')
 
+
+print 'Reading Files started..'
 ar_lines=ar_file.readlines()
 fa_lines=fa_file.readlines()
+print 'Reading Files ended..'
 
 if len(ar_lines) !=  len(fa_lines):
-    print 'Not Equal lines'
+    print 'Not Equal lines,is this corpus parallel?'
     exit(-1)
 
 farsi_out_file=farsi_file+'.filtered'
@@ -31,6 +33,7 @@ fa_out_file=io.open(farsi_out_file,'w',encoding='utf-8')
 ar_out_file=io.open(arabic_out_file,'w',encoding='utf-8')
 
 remove_line_counter=0
+print 'Filtering process starts...'
 for i,line in enumerate(ar_lines):
     if filters.isArabic(line) and filters.isFarsi(fa_lines[i]):
         fa_out_file.write(fa_lines[i]+'\r\n')
@@ -41,3 +44,4 @@ fa_out_file.close()
 ar_out_file.close()
 
 print 'Number of removed line:',remove_line_counter
+print 'DONE!'
