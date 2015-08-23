b0d1508f051899ab114e87d30d399df0566dfa67
Amir Pouya Aghasadeghi <amirpouya.a@gmail.com>
2015-08-23 11:38:50 +0430

    Okay readall was incorrect:|

--- xml_processing/LDC2XML.py
+++ xml_processing/LDC2XML.py
@@ -8,15 +8,13 @@ target_file=sys.argv[2]
 try:
     source_tree=ET.parse(source_file)
 except:
-    source_file=open(source_file,'r')
-    source_file.readall(source_file)
+    source_file=open(source_file,'r').read()
     source_file='<corpus>'+source_file+'</corpus>'
     source_tree=ET.fromstring(source_file)
 try:
     target_tree=ET.parse(target_file)
 except:
-    target_file=open(target_file,'r')
-    target_file.readall(target_file)
+    target_file=open(target_file,'r').read()
     target_file='<corpus>'+target_file+'</corpus>'
     target_tree=ET.fromstring(target_file)
 
