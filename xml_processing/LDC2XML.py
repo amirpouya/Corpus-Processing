5d11d4da5be648b7d2ce191146bf3dfeb1ec2694
Amir Pouya Aghasadeghi <amirpouya.a@gmail.com>
2015-08-23 11:29:47 +0430

    checking for root
    if xml does not have root add a root

--- xml_processing/LDC2XML.py
+++ xml_processing/LDC2XML.py
@@ -5,8 +5,20 @@ import sys
 source_file=sys.argv[1]
 target_file=sys.argv[2]
 
-source_tree=ET.parse(source_file)
-target_tree=ET.parse(target_file)
+try:
+    source_tree=ET.parse(source_file)
+except:
+    source_file=open(source_file,'r')
+    source_file.read(source_file)
+    source_file='<corpus>'+source_file+'</corpus>'
+    source_tree=ET.fromstring(source_file)
+try:
+    target_tree=ET.parse(target_file)
+except:
+    target_file=open(target_file,'r')
+    target_file.read(target_file)
+    target_file='<corpus>'+target_file+'</corpus>'
+    target_tree=ET.fromstring(target_file)
 
 source_root=source_tree.getroot()
 target_root=target_tree.getroot()
