e44104ea679d46663d64ce85bb6c999c6937e85e
Amir Pouya Aghasadeghi <amirpouya.a@gmail.com>
2015-08-23 11:36:31 +0430

    change read to readall

--- xml_processing/LDC2XML.py
+++ xml_processing/LDC2XML.py
@@ -9,14 +9,14 @@ try:
     source_tree=ET.parse(source_file)
 except:
     source_file=open(source_file,'r')
-    source_file.read(source_file)
+    source_file.readall(source_file)
     source_file='<corpus>'+source_file+'</corpus>'
     source_tree=ET.fromstring(source_file)
 try:
     target_tree=ET.parse(target_file)
 except:
     target_file=open(target_file,'r')
-    target_file.read(target_file)
+    target_file.readall(target_file)
     target_file='<corpus>'+target_file+'</corpus>'
     target_tree=ET.fromstring(target_file)
 
